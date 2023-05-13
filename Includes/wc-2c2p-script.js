7731900cd98993621f9268c0985343dc7dce0f0e
Menn <m@seedwebs.com>
2023-05-13 18:38:24 +0700

    Fix: Uncaught TypeError: $(...).live is not a function
    .live has been removed in jQuery 1.9; use on() instead.

--- Includes/wc-2c2p-script.js
+++ Includes/wc-2c2p-script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
     //Hide remove button when user select first option from dropdown (l'll use my new card)
-    $('#wc_2c2p_stored_card').live('change', function() {
+    $('#wc_2c2p_stored_card').on('change', function() {
         var value = $('#wc_2c2p_stored_card').val();
 
         if (value === "0") {
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
     });
 
     //Button click event
-    $('#btn_2c2p_remove').live('click', function() {
+    $('#btn_2c2p_remove').on('click', function() {
 		if($("#wc_2c2p_stored_card").val() === "0"){
 			alert("Please select card number to delete.");
 			return;
